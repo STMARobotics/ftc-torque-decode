79104894aba8d19944c64cc0863067fa4e40db34
STMA Robotics <robotics@stmarobotics.org>
2025-10-10 19:05:53 -0500

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveProgram_FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveProgram_FieldCentric.java
@@ -61,7 +61,7 @@ public class DriveProgram_FieldCentric extends OpMode {
         if (gamepad1.dpadLeftWasPressed() && speed == 0.75) {
              speed =  0.4;
         }
-        if (gamepad1.dpad_left && speed == 0.4) {
+        else if (gamepad1.dpadLeftWasPressed() && speed == 0.4) {
             speed = 0.75;
         }
 
